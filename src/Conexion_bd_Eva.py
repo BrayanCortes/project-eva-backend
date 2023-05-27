4b51077928df451dd5844d5cabb10c66d712a547
Brayan Cortes <bralejo_789@gmail.com>
2023-05-27 18:36:54 -0500

    feat: add results to update

--- src/Conexion_bd_Eva.py
+++ src/Conexion_bd_Eva.py
@@ -35,8 +35,8 @@ def guardar_datos(name,code_student,email, Question1, Question2, Question3, anal
         print(registro)
         if registro:
             # Si el registro ya existe, muestra los datos en la aplicación Tkinter
-            consulta = "UPDATE Data_Test2 SET Email = %s, nombre = %s, respuesta1 = %s, respuesta2 = %s, respuesta3 = %s WHERE codigo = %s"
-            valores = (email,name,Question1, Question2, Question3, code_student)
+            consulta = "UPDATE Data_Test2 SET Email = %s, nombre = %s, respuesta1 = %s, respuesta2 = %s, respuesta3 = %s, resultados =%s WHERE codigo = %s"
+            valores = (email,name,Question1, Question2, Question3, code_student,analisis)
             cursor.execute(consulta, valores)
             print(f"Update Student with code:{code_student} and Name:{name}")
         else:
